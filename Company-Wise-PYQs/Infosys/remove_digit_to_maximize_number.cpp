eeed2d0a9abc013e2e4702225fb8f698484396e8
mohanteja781112 <doddimohanteja711@gmail.com>
2026-07-07 19:00:15 +0530

    Initial commit

---
+++ Company-Wise-PYQs/Infosys/remove_digit_to_maximize_number.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string number;
+    char digit; 
+    cin >> number >> digit;
+    string ans="";
+    for(int i=0;i<number.size();i++){
+        if(number[i]==digit){
+            string temp=number.substr(0,i)+number.substr(i+1);
+            ans=max(ans,temp);
+        }
+    }
+    cout<<ans<<endl;
+}
