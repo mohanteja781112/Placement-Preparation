e55145dacee248fc9dcda60edaa31b7a89e09895
mohanteja781112 <doddimohanteja711@gmail.com>
2026-07-08 18:08:45 +0530

    Maximum_Prefix_XOR_Sum_in_Circular_Array

---
+++ Placement Preparation/Company-Wise-PYQs/Infosys/Maximum_Prefix_XOR_Sum_in_Circular_Array.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; cin>>n;
+    vector<int>a(n);;
+    for(int i=0;i<n;i++) cin>>a[i];
+    long long ans=0;
+    for(int start=0;start<n;start++){
+        long long sum=0;
+        int xr=0;
+        // clock-wise   
+        for(int k=0;k<n;k++){
+            int idx=(start+k)%n;
+            xr^=a[idx];
+            sum+=xr;
+        }
+        ans=max(ans,sum);
+
+        //anti clock-wise
+        sum=0;
+        xr=0;
+        for(int k=0;k<n;k++){
+            int idx=(start-k+n)%n;
+            xr^=a[idx];
+            sum+=xr;
+        }
+        ans=max(ans,sum);
+    }
+    cout<<ans; 
+}
